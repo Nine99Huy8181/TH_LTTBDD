eb704393362d0c53af2bfe6a31585fb4dcbb4fb2
NguyenQuocHuyGITHUB <122158094+Nine99Huy8181@users.noreply.github.com>
2025-08-22 22:24:05 +0700

    bai 18

---
+++ TH_Tuan01/dist/bai18.js
@@ -0,0 +1,18 @@
+"use strict";
+class MathUtil {
+    static add(a, b) {
+        return a + b;
+    }
+    static subtract(a, b) {
+        return a - b;
+    }
+    static multiply(a, b) {
+        return a * b;
+    }
+    static divide(a, b) {
+        if (b === 0) {
+            throw new Error("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
