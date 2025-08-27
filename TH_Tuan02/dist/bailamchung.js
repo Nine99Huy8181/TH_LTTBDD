6f6662fd8aaec5ee29c9790b996468353cc1d759
H82M19\Student <Student>
2025-08-27 08:08:26 +0700

    bai 05

--- TH_Tuan02/dist/bailamchung.js
+++ TH_Tuan02/dist/bailamchung.js
@@ -1,16 +1,25 @@
 "use strict";
 //4
-const excercise4 = () => {
-    const randomPromise = new Promise((resolve, reject) => {
-        const random = Math.random();
+// const excercise4 = ():void => {
+//     const randomPromise = new Promise<number>((resolve, reject) => {
+//         const random = Math.random();
+//         setTimeout(() => {
+//             if(random > 0.5) resolve(random)
+//             else reject(new Error("Random number too low"))
+//         }, 500)
+//     })
+//     randomPromise.then((rs) => console.log(rs))
+//     .catch((error) => console.log(error.message)
+//     )
+// }
+// excercise4();
+//05
+const simulateTask = (time) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
-            if (random > 0.5)
-                resolve(random);
-            else
-                reject(new Error("Random number too low"));
-        }, 500);
+            resolve("Done!");
+        }, time);
     });
-    randomPromise.then((rs) => console.log(rs))
-        .catch((error) => console.log(error.message));
 };
-excercise4();
+const bai05 = simulateTask(1000);
+bai05.then((rs) => console.log(rs)).catch((error) => console.log(error.message));
--- TH_Tuan02/src/bailamchung.ts
+++ TH_Tuan02/src/bailamchung.ts
@@ -1,16 +1,30 @@
 //4
-const excercise4 = ():void => {
-    const randomPromise = new Promise<number>((resolve, reject) => {
-        const random = Math.random();
-        setTimeout(() => {
-            if(random > 0.5) resolve(random)
-            else reject(new Error("Random number too low"))
-        }, 500)
-    })
+// const excercise4 = ():void => {
+//     const randomPromise = new Promise<number>((resolve, reject) => {
+//         const random = Math.random();
+//         setTimeout(() => {
+//             if(random > 0.5) resolve(random)
+//             else reject(new Error("Random number too low"))
+//         }, 500)
+//     })
 
-    randomPromise.then((rs) => console.log(rs))
-    .catch((error) => console.log(error.message)
-    )
+//     randomPromise.then((rs) => console.log(rs))
+//     .catch((error) => console.log(error.message)
+//     )
+// }
+
+// excercise4();
+
+
+//05
+const simulateTask = (time: number):Promise<string> => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Done!")
+        }, time)
+    })
 }
 
-excercise4();
+const bai05 = simulateTask(1000);
+bai05.then((rs) => console.log(rs)).catch((error) => console.log(error.message))
+
